6f71bfe1fd8a9c59e65af0154a4911a0402a1d96
juuliabellcom <juuliadevadze@gmail.com>
2019-07-04 10:47:22 +0300

    service_links   7.x-2.1  -> 7.x-2.4

--- public_html/sites/all/modules/service_links/service_links.api.php
+++ public_html/sites/all/modules/service_links/service_links.api.php
@@ -3,10 +3,13 @@
 /**
  * @file
  * Provides documentation for the Service Links API.
+ *
+ * Look the online documentation for more info.
  */
 
 /**
  * Obtains all available service links.
+ *
  * @return
  *   An array containing all service links, keyed by name.
  */
@@ -18,27 +21,41 @@ function hook_service_links() {
     'name' => 'MyService',
     // A short description for the link.
     'description' => t('Share this post on MyService'),
-    // The link's URL. Available values are:
-    //   <encoded-url>, <encoded-title>, <encoded-teaser>, <teaser>, <short-url>,
-    // <source>, <node-id>, <url>, and <title>.
+    // The service URL and its params.
     'link' => 'http://example.com/?url=<encoded-url>&title=<encoded-title>&summary=<encoded-teaser>',
-    // The service's small share icon. This is the relative path from Drupal's
-    // base path, or the absolute URL.
+    // The service icon. The id name and .png extension is used as default.
     'icon' => drupal_get_path('module', 'myservice') .'/myservice.png',
     // Any additional attributes to apply to the element.
     'attributes' => array(
-      'class' => 'myservice-special-class', // A special class.
+      'class' => array('myservice-special-class'), // A special class.
       'style' => 'text-decoration: underline;', // Apply any special inline styles.
     ),
-    // JavaScript that is added when this link is processed.
+    // JavaScript to add when this link is processed, can be a string or an array.
     'javascript' => drupal_get_path('module', 'myservice') .'/myservice.js',
-    // CSS that is added when this link is processed.
+    // CSS to add when this link is processed, can be a string or an array.
     'css' => drupal_get_path('module', 'myservice') .'/myservice.css',
+    // A PHP function invoked before the link is created, useful to add new tags.
+    'preset' => 'myservice_preset',
     // A PHP function callback that is invoked when the link is created.
     'callback' => 'myservice_callback',
   );
 
   return $links;
+}
+
+/**
+ * Example of preset function.
+ *
+ * @param $service
+ *   The service that is being used.
+ * @param $settings
+ *   An array containing all the settings used.
+ * @param $node
+ *   An object containt the current node.
+ */
+function my_service_preset(&$service, &$settings, $node = NULL) {
+  $settings['tags']['new-tag'] = '<new-tag>';
+  $settings['subst']['new-tag'] = check_plain(variable_get('some_variable', 'default'));
 }
 
 /**
@@ -63,5 +80,29 @@ function hook_service_links_alter(&$links) {
   if (isset($links['myservice'])) {
     // Change the icon of MyService.
     $links['myservice']['icon'] = 'http://drupal.org/misc/favicon.ico';
+  }
+}
+
+/**
+ * Allows alteration of the service link settings for a given node.
+ *
+ * @param array $settings
+ *   Reference to the array of service link settings for a node.
+ * @param node
+ *   The fully loaded node object that the settings correspond to.
+ *
+ * @see drupal_alter();
+ * @see _service_links_get_tags();
+ */
+function hook_service_links_node_settings_alter(&$settings, $node) {
+  // Use a site-specific URL shortener.
+  $short_url = "http://example.com/$node->nid";
+  $settings['subst']['short-url'] = $short_url;
+  $settings['subst']['encoded-short-url'] = urlencode($short_url);
+  $settings['subst']['raw-encoded-short-url'] = urlencode($short_url);
+
+  // Hide a specific service on nodes of a specific type.
+  if ($node->type == 'notweets') {
+    unset($settings['link_show']['twitter']);
   }
 }
